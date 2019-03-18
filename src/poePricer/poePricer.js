89573153be52583343f3a343bbe70bf6819b23ab
mahhov <mahhov1@gmail.com>
2019-03-18 15:43:34 -0700

    ctrl+shift:h for /hideout

--- src/poePricer/poePricer.js
+++ src/poePricer/poePricer.js
@@ -11,15 +11,16 @@ TrayHelper.createExitTray(trayIcon, 'Poe Pricer');
 let clipboard = new Clipboard();
 let viewHandle = new ViewHandle();
 
+let releaseKeys = keys => {
+	KeySender.startBatch();
+	keys.forEach(key => KeySender.batchTypeKey(key, 0, KeySender.BATCH_EVENT_KEY_UP));
+	KeySender.sendBatch();
+};
+
 let lastClipboardInput;
 
 let startPricer = async () => {
-	KeySender
-		.startBatch()
-		.batchTypeKey('control', 0, KeySender.BATCH_EVENT_KEY_UP)
-		.batchTypeKey('shift', 0, KeySender.BATCH_EVENT_KEY_UP)
-		.batchTypeKey('x', 0, KeySender.BATCH_EVENT_KEY_UP)
-		.sendBatch();
+	releaseKeys(['control', 'shift', 'X']);
 	let clipboardInput = await clipboard.copy();
 	if (viewHandle.visible && clipboardInput === lastClipboardInput)
 		viewHandle.hide();
@@ -31,8 +32,17 @@ let startPricer = async () => {
 	}
 };
 
+let hideout = () => {
+	releaseKeys(['control', 'shift', 'h']);
+	KeySender.sendKeys(['enter']);
+	KeySender.startBatch().batchTypeText('/hideout').sendBatch();
+	KeySender.sendKey('enter');
+};
+
 ShortcutListener.add('Control+Shift+X', startPricer);
+ShortcutListener.add('Control+Shift+H', hideout);
 
 // todo only apply when in appropriate window title
-// todo /h for hideout
 // todo unlock macro
+// todo sizing
+// todo uniques showing shaper/elder base prices
