b40d3136f04046af6bd22c379ede6809419c17f6
manuk <mahhov1@gmail.com>
2019-03-21 07:13:06 -0700

    use c# constatns for action values -1 to -4

--- src/base/keySender/keySender.ps1
+++ src/base/keySender/keySender.ps1
@@ -4,6 +4,11 @@ Add-Type @"
     public static class UserWindows {
         [DllImport("user32.dll")] static extern void keybd_event(byte bVk, byte bScan, uint dwFlags, UIntPtr dwExtraInfo);
 
+        private const int ACTION_RELEASE = -1;
+        private const int ACTION_PRESS = -2;
+        private const int ACTION_TYPE = -3;
+        private const int ACTION_COMBO = -4;
+
         public static void keys(int[] keys) {
             int action = 0;
             int i = 0;
@@ -12,23 +17,23 @@ Add-Type @"
             while (i < keys.Length) {
                 int key = keys[i];
                 if (key < 0) {
-                    if (action == -4) {
+                    if (action == ACTION_COMBO) {
                         i = lastActionI;
-                        action = -1;
+                        action = ACTION_RELEASE;
                     } else
                         action = key;
                     lastActionI = i;
                 } else {
                     byte vk = (byte) key;
                     switch (action) {
-                        case -1: // release
+                        case ACTION_RELEASE:
                             keybd_event(vk, 0, 0x2, UIntPtr.Zero);
                             break;
-                        case -2: // press
-                        case -4: // combo
+                        case ACTION_PRESS:
+                        case ACTION_COMBO:
                             keybd_event(vk, 0, 0x0, UIntPtr.Zero);
                             break;
-                        case -3: // type
+                        case ACTION_TYPE:
                             keybd_event(vk, 0, 0x0, UIntPtr.Zero);
                             keybd_event(vk, 0, 0x2, UIntPtr.Zero);
                             break;
