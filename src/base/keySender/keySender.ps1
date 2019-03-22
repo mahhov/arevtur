37fa9c281a22f10f04abba71e639641e1f14b82c
manuk <mahhov1@gmail.com>
2019-03-22 15:20:49 -0700

    tab->space keySender.ps1

--- src/base/keySender/keySender.ps1
+++ src/base/keySender/keySender.ps1
@@ -6,45 +6,45 @@ Add-Type @"
 	public static class UserWindows {
 		[DllImport("user32.dll")] static extern void keybd_event(byte bVk, byte bScan, uint dwFlags, UIntPtr dwExtraInfo);
 
-        private const int ACTION_RELEASE = -1;
-        private const int ACTION_PRESS = -2;
-        private const int ACTION_TYPE = -3;
-        private const int ACTION_COMBO = -4;
+		private const int ACTION_RELEASE = -1;
+		private const int ACTION_PRESS = -2;
+		private const int ACTION_TYPE = -3;
+		private const int ACTION_COMBO = -4;
 
         public static void keys(int[] keys) {
-            int action = 0;
-            int i = 0;
-            int lastActionI = 0;
+			int action = 0;
+			int i = 0;
+			int lastActionI = 0;
 
-            while (i < keys.Length) {
-                int key = keys[i];
-                if (key < 0) {
-                    if (action == ACTION_COMBO) {
-                        i = lastActionI;
-                        action = ACTION_RELEASE;
-                    } else
-                        action = key;
-                    lastActionI = i;
-                } else {
-                    byte vk = (byte) key;
-                    switch (action) {
-                        case ACTION_RELEASE:
-                            keybd_event(vk, 0, 0x2, UIntPtr.Zero);
-                            break;
-                        case ACTION_PRESS:
-                        case ACTION_COMBO:
-                            keybd_event(vk, 0, 0x0, UIntPtr.Zero);
-                            break;
-                        case ACTION_TYPE:
-                            keybd_event(vk, 0, 0x0, UIntPtr.Zero);
-                            keybd_event(vk, 0, 0x2, UIntPtr.Zero);
-                            break;
-                    }
-                }
-                i++;
-            }
-        }
-    }
+			while (i < keys.Length) {
+				int key = keys[i];
+				if (key < 0) {
+					if (action == ACTION_COMBO) {
+						i = lastActionI;
+						action = ACTION_RELEASE;
+					} else
+						action = key;
+					lastActionI = i;
+				} else {
+					byte vk = (byte) key;
+					switch (action) {
+						case ACTION_RELEASE:
+							keybd_event(vk, 0, 0x2, UIntPtr.Zero);
+							break;
+						case ACTION_PRESS:
+						case ACTION_COMBO:
+							keybd_event(vk, 0, 0x0, UIntPtr.Zero);
+							break;
+						case ACTION_TYPE:
+							keybd_event(vk, 0, 0x0, UIntPtr.Zero);
+							keybd_event(vk, 0, 0x2, UIntPtr.Zero);
+							break;
+					}
+				}
+				i++;
+			}
+		}
+	}
 "@
 
 foreach ($i in $input) {
