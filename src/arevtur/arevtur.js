ff9c717d4531b15c25d625d5c0b3805d1fd267c5
manuk <mahhov1@gmail.com>
2020-07-10 17:10:50 -0700

    extract new class ElectornWindow  from arevtur

--- src/arevtur/arevtur.js
+++ src/arevtur/arevtur.js
@@ -1,38 +1,6 @@
 const path = require('path');
-const {app, BrowserWindow} = require('electron');
-const {appReadyPromise} = require('js-desktop-base');
+const ElectronWindow = require('../services/ElectronWindow');
 
-// workaround for electron bug
-// https://github.com/electron/electron/issues/22119
-app.allowRendererProcessReuse = false;
+let window = new ElectronWindow('Arevtur', path.resolve(__dirname, 'trade.html'));
 
-let window = appReadyPromise.then(() => {
-	let window = new BrowserWindow({
-		width: 1800,
-		height: 1000,
-		show: false,
-		webPreferences: {nodeIntegration: true},
-	});
-	window.setMenu(null);
-	window.loadFile(path.resolve(__dirname, 'trade.html'));
-
-	window.on('close', e => {
-		e.preventDefault();
-		window.hide();
-	});
-
-	return window;
-});
-
-let showView = async () =>
-	(await window).show();
-
-let showDevTools = async () =>
-	(await window).webContents.openDevTools();
-
-module.exports = {
-	trayOptions: [
-		{label: 'Arevtur', click: showView},
-		{label: 'Arevtur - dev', click: showDevTools},
-	],
-};
+module.exports = {trayOptions: window.trayOptions};
---
+++ src/services/ElectronWindow.js
@@ -0,0 +1,47 @@
+const {app, BrowserWindow} = require('electron');
+const {appReadyPromise} = require('js-desktop-base');
+
+// workaround for electron bug
+// https://github.com/electron/electron/issues/22119
+app.allowRendererProcessReuse = false;
+
+class ElectronWindow {
+	constructor(name, htmlPath, width = 1800, height = 1000) {
+		this.name = name;
+
+		this.window = appReadyPromise.then(() => {
+			let window = new BrowserWindow({
+				width,
+				height,
+				show: false,
+				webPreferences: {nodeIntegration: true},
+			});
+			window.setMenu(null);
+			window.loadFile(htmlPath);
+
+			window.on('close', e => {
+				e.preventDefault();
+				window.hide();
+			});
+
+			return window;
+		});
+	}
+
+	async showView() {
+		(await this.window).show();
+	}
+
+	async showDevTools() {
+		(await this.window).webContents.openDevTools();
+	}
+
+	get trayOptions() {
+		return [
+			{label: `${this.name}`, click: () => this.showView()},
+			{label: `${this.name} - dev`, click: () => this.showDevTools()},
+		];
+	}
+}
+
+module.exports = ElectronWindow;
