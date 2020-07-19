f345ef8ec9d4221c3134d8d7f48d5f114064ab22
manuk <mahhov1@gmail.com>
2020-07-19 11:15:29 -0700

    rename trade.html to arevtur.html

--- src/arevtur/arevtur.js
+++ src/arevtur/arevtur.js
@@ -1,6 +1,6 @@
 const path = require('path');
 const ElectronWindow = require('../services/ElectronWindow');
 
-let window = new ElectronWindow('Arevtur', path.resolve(__dirname, 'trade.html'));
+let window = new ElectronWindow('Arevtur', path.resolve(__dirname, 'arevtur.html'));
 
 module.exports = {trayOptions: window.trayOptions};
