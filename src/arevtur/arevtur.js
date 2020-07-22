5e9e7510c05e8bd19434e26fa666cd6f593d9f94
manuk <mahhov1@gmail.com>
2020-07-22 09:48:38 -0700

    overhaul path select to use native os dialog in order to support default path

--- src/arevtur/arevtur.js
+++ src/arevtur/arevtur.js
@@ -1,6 +1,11 @@
 const path = require('path');
+const {ipcMain, dialog} = require('electron');
 const ElectronWindow = require('../services/ElectronWindow');
 
 let window = new ElectronWindow('Arevtur', path.resolve(__dirname, 'arevtur.html'));
+
+ipcMain.handle('open-dialog', async (event, arg) =>
+	dialog.showOpenDialog(await window.window, arg));
+
 
 module.exports = {trayOptions: window.trayOptions};
--- src/arevtur/xElements/pathSelect/PathSelect.js
+++ src/arevtur/xElements/pathSelect/PathSelect.js
@@ -1,9 +1,10 @@
 const {XElement, importUtil} = require('xx-element');
 const {template, name} = importUtil(__filename);
+const {ipcRenderer} = require('electron');
 
 customElements.define(name, class extends XElement {
 	static get attributeTypes() {
-		return {placeholder: {}, path: {}, directory: {boolean: true}};
+		return {placeholder: {}, path: {}, defaultPath: {}, directory: {boolean: true}, extension: {}};
 	}
 
 	static get htmlTemplate() {
@@ -11,12 +12,18 @@ customElements.define(name, class extends XElement {
 	}
 
 	connectedCallback() {
-		this.$('input').addEventListener('input', () => {
-			let path = this.$('input').files[0].path;
-			this.path = this.directory ? path.replace(/\\[^\\]*$/, '') : path;
-			this.emit('selected');
+		this.$('#path').addEventListener('click', async () => {
+			let dialogResponse = await ipcRenderer.invoke('open-dialog', {
+				title: this.placeholder,
+				defaultPath: this.path || this.defaultPath || '',
+				properties: this.directory ? ['openDirectory'] : [],
+				filters: [{name: '', extensions: [this.extension]}],
+			});
+			if (!dialogResponse.canceled) {
+				this.path = dialogResponse.filePaths[0];
+				this.emit('selected');
+			}
 		});
-		this.$('#path').addEventListener('click', () => this.$('input').click());
 		this.$('#clear').addEventListener('click', () => {
 			this.path = '';
 			this.emit('selected');
@@ -28,15 +35,11 @@ customElements.define(name, class extends XElement {
 	}
 
 	set path(value) {
-		this.$('input').value = '';
 		this.updateText();
-	}
-
-	set directory(value) {
-		this.$('input').webkitdirectory = value;
 	}
 
 	updateText() {
 		this.$('#path').textContent = this.path?.slice(-30) || this.placeholder;
+		this.$('#path').title = this.path;
 	}
 });
