83eb6d48ec8aa6e70e8ae559c294ecfd623107d1
mahhov1 <mahhov1@gmail.com>
2024-08-17 15:17:05 -0700

    fix Searcher case sensitivity and ! operator

--- src/arevtur/Searcher.js
+++ src/arevtur/Searcher.js
@@ -1,19 +1,31 @@
+const assert = require('assert');
+
 class Searcher {
 	constructor(searchText) {
 		this.terms = searchText
+			.toLowerCase()
 			.split(/\s+/)
-			.map(word => word.startsWith('!') ? [word, false] : [word, true]);
+			.filter(v => v)
+			.map(word => word.startsWith('!') ? [word.slice(1), false] : [word, true]);
 	}
 
 	test(string) {
+		string = string.toLowerCase();
 		return this.terms.every(([word, include]) =>
 			string.includes(word) === include);
 	}
 
 	testMulti(strings) {
+		strings = strings.map(string => string.toLowerCase());
 		return this.terms.every(([word, include]) =>
 			strings.some(string => string.includes(word)) === include);
 	}
 }
+
+let searcher = new Searcher('cOld rEs % !fIR');
+assert(searcher.test('+5% cold resistance'));
+assert(searcher.test('+5% cold and lightning resistance'));
+assert(!searcher.test('+5% cold and fire resistance'));
+assert(!searcher.test('lots of cold resistance'));
 
 module.exports = Searcher;
