850f6c2fd7265f5a165148696bdfe38ccb810e3c
mahhov1 <mahhov1@gmail.com>
2024-08-17 17:03:25 -0700

    add '=' filtering to Searcher - i.e. restrict allowed words in search string

--- src/arevtur/Searcher.js
+++ src/arevtur/Searcher.js
@@ -1,24 +1,52 @@
 const assert = require('assert');
 
+// case insensitive
+// not necessarily at a word boundary
+// not necessarily in order
+// 'x y' means 'x' and 'y' must be contained in 'string'
+// 'x !y' means 'x' must be contained in 'string', but 'y' must not be
+// 'x = y' means 'x' must be contained in 'string', and string must only consist of 'x' and 'y'
 class Searcher {
 	constructor(searchText) {
-		this.terms = searchText
+		this.requiredWords = [];
+		this.allowedWords = [];
+		this.unallowedWords = [];
+
+		let equalEncountered = false;
+		searchText
 			.toLowerCase()
 			.split(/\s+/)
 			.filter(v => v)
-			.map(word => word.startsWith('!') ? [word.slice(1), false] : [word, true]);
+			.forEach(word => {
+				if (word === '=') {
+					equalEncountered = true;
+					return;
+				}
+				if (equalEncountered)
+					this.allowedWords.push(word);
+				else if (word.startsWith('!'))
+					this.unallowedWords.push(word.slice(1));
+				else
+					this.requiredWords.push(word);
+			});
+		if (this.allowedWords.length)
+			this.allowedWords.push(...this.requiredWords);
 	}
 
 	test(string) {
 		string = string.toLowerCase();
-		return this.terms.every(([word, include]) =>
-			string.includes(word) === include);
+		return (
+			this.requiredWords.every(word => string.includes(word)) &&
+			(!this.allowedWords.length || string
+				.split(/\s+/)
+				.filter(v => v)
+				.every(stringWord => this.allowedWords.includes(stringWord))) &&
+			this.unallowedWords.every(word => !string.includes(word))
+		);
 	}
 
 	testMulti(strings) {
-		strings = strings.map(string => string.toLowerCase());
-		return this.terms.every(([word, include]) =>
-			strings.some(string => string.includes(word)) === include);
+		return strings.some(string => this.test(string));
 	}
 }
 
@@ -27,5 +55,10 @@ assert(searcher.test('+5% cold resistance'));
 assert(searcher.test('+5% cold and lightning resistance'));
 assert(!searcher.test('+5% cold and fire resistance'));
 assert(!searcher.test('lots of cold resistance'));
+searcher = new Searcher('cold = resist fire');
+assert(searcher.test('cold resist'));
+assert(!searcher.test('fire resist'));
+assert(searcher.test('cold fire resist'));
+assert(!searcher.test('cold fire lightning resist'));
 
 module.exports = Searcher;
