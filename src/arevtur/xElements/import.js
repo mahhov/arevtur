98b06b5373135cfba4e6f252611dce0c8d595946
mahhov1 <mahhov1@gmail.com>
2024-08-19 13:14:00 -0700

    fix x-element import order to avoid dropping attributes

--- src/arevtur/xElements/import.js
+++ src/arevtur/xElements/import.js
@@ -1,17 +1,27 @@
+// each group can only depend on the groups above it
+
 require('./arrangableList/ArrangableList');
 require('./autocompleteInput/AutocompleteInput');
-require('./chart/Chart');
 require('./configToggleButton/ConfigToggleButton');
 require('./formattedText/FormattedText');
-require('./inputBuild/InputBuild');
 require('./inputImportTradeSearchUrl/InputImportTradeSearchUrl');
 require('./inputSet/InputSet');
-require('./inputTradeParams/InputTradeParams');
-require('./inputs/Inputs');
-require('./itemListing/ItemListing');
-require('./multiSelectLined/MultiSelectLined');
 require('./numericInput/NumericInput');
 require('./pathSelect/PathSelect');
-require('./queryProperty/QueryProperty');
-require('./results/Results');
-require('./tooltipButton/TooltipButton');
+
+require('./inputBuild/InputBuild'); // x-path-select, x-numeric-input
+require('./multiSelectLined/MultiSelectLined'); // x-autocomplete-input
+require('./tooltipButton/TooltipButton'); // x-formatted-text
+
+require('./chart/Chart'); // x-tooltip-button
+// x-autocomplete-input, x-numeric-input, x-tooltip-button, x-multi-select-lined, x-arrangable-list
+require('./inputTradeParams/InputTradeParams');
+require('./itemListing/ItemListing'); // x-tooltip-button
+require('./queryProperty/QueryProperty'); // x-autocomplete-input, x-numeric-input, x-tooltip-button
+
+// x-autocomplete-input,  x-config-toggle-button,  x-input-build,  x-input-import-trade,
+// x-arrangable-list,  x-input-trade-params
+require('./inputs/Inputs');
+require('./results/Results'); // x-autocomplete-input, x-chart
+
+
