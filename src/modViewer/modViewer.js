bc3b9e0219c33bea5d58aa4afcb906375c72df79
mahhov1 <mahhov1@gmail.com>
2024-08-19 08:01:50 -0700

    remove ModViewer and axios

--- src/modViewer/modViewer.js
+++
@@ -1,6 +0,0 @@
-const path = require('path');
-const ElectronWindow = require('../services/ElectronWindow');
-
-let window = new ElectronWindow('Mod Viewer', path.resolve(__dirname, 'modViewer.html'));
-
-module.exports = window;
