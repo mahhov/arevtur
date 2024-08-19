c6526e2c151a8ef1c2cf9571a0515a59f16bcf06
mahhov1 <mahhov1@gmail.com>
2024-08-19 13:15:48 -0700

    fix typo arrangableList -> arrangeableList

--- src/arevtur/xElements/import.js
+++ src/arevtur/xElements/import.js
@@ -1,6 +1,6 @@
 // each group can only depend on the groups above it
 
-require('./arrangableList/ArrangableList');
+require('./arrangeableList/ArrangeableList');
 require('./autocompleteInput/AutocompleteInput');
 require('./configToggleButton/ConfigToggleButton');
 require('./formattedText/FormattedText');
@@ -14,13 +14,13 @@ require('./multiSelectLined/MultiSelectLined'); // x-autocomplete-input
 require('./tooltipButton/TooltipButton'); // x-formatted-text
 
 require('./chart/Chart'); // x-tooltip-button
-// x-autocomplete-input, x-numeric-input, x-tooltip-button, x-multi-select-lined, x-arrangable-list
+// x-autocomplete-input, x-numeric-input, x-tooltip-button, x-multi-select-lined, x-arrangeable-list
 require('./inputTradeParams/InputTradeParams');
 require('./itemListing/ItemListing'); // x-tooltip-button
 require('./queryProperty/QueryProperty'); // x-autocomplete-input, x-numeric-input, x-tooltip-button
 
 // x-autocomplete-input,  x-config-toggle-button,  x-input-build,  x-input-import-trade,
-// x-arrangable-list,  x-input-trade-params
+// x-arrangeable-list,  x-input-trade-params
 require('./inputs/Inputs');
 require('./results/Results'); // x-autocomplete-input, x-chart
 
