308f4279ca7162e31ce5306b3c55a081e09b9b30
mahhov1 <mahhov1@gmail.com>
2024-09-03 06:59:00 -0700

    ga measure query time

--- src/util/util.js
+++ src/util/util.js
@@ -37,6 +37,8 @@ let flattenObject = (obj, separator = '_') => {
 	}, {});
 };
 
+let randInt = n => Math.floor(Math.random() * n);
+
 module.exports = {
 	minIndex: array => array.indexOf(Math.min(...array)),
 	maxIndex: array => array.indexOf(Math.max(...array)),
@@ -45,5 +47,6 @@ module.exports = {
 	deepCopy,
 	flattenObject,
 	transpose: a => a[0].map((_, i) => a.map(v => v[i])),
+	randInt,
 	// todo[low] move un-classed & static methods like decode64 here
 };
