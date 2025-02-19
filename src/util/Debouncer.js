59f5ef04c4884554d20f4881a886aba6cc41bc26
mahhov1 <mahhov1@gmail.com>
2025-02-18 16:47:05 -0800

    update debouncer to count from last run instead of last request

--- src/util/Debouncer.js
+++ src/util/Debouncer.js
@@ -2,15 +2,25 @@ class Debouncer {
 	constructor(handler, delay) {
 		this.handler = handler;
 		this.delay = delay;
-		this.timeout = null;
+		this.scheduled = false;
+		this.lastRun = 0;
 	}
 
 	request() {
-		clearTimeout(this.timeout);
-		this.timeout = setTimeout(() => this.run(), this.delay);
+		if (this.scheduled)
+			return;
+		let delay = this.delay - (Date.now() - this.lastRun);
+		if (delay < 0)
+			this.run();
+		else {
+			this.scheduled = true;
+			setTimeout(() => this.run(), delay);
+		}
 	}
 
 	run() {
+		this.scheduled = false;
+		this.lastRun = Date.now();
 		this.handler();
 	}
 }
