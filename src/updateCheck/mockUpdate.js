76ee16adf7cce0f6028d8d18c71b6ed40887804f
mahhov1 <mahhov1@gmail.com>
2025-09-10 21:12:53 -0700

    verify updater working

---
+++ src/updateCheck/mockUpdate.js
@@ -0,0 +1,25 @@
+module.exports = {
+	'tag': 'v8.12.0',
+	'version': '8.12.0',
+	'files': [
+		{
+			'url': 'arevtur-8.12.0-win-x64.exe',
+			'sha512': 'b9iGCvjfVKjpw+fyKxxQqQMDpC2Ce1UYoQM0sq38nXb3o6fDB+g1xQmGOw9Ge5+DqAp9mYZgTU4UJyN//PuYoQ==',
+			'size': 84690446
+		}
+	],
+	'path': 'arevtur-8.12.0-win-x64.exe',
+	'sha512': 'b9iGCvjfVKjpw+fyKxxQqQMDpC2Ce1UYoQM0sq38nXb3o6fDB+g1xQmGOw9Ge5+DqAp9mYZgTU4UJyN//PuYoQ==',
+	'releaseDate': '2025-09-10T16:09:06.889Z',
+	'releaseName': '8.12.0',
+	'releaseNotes': [
+		{
+			'version': '8.12.0',
+			'note': '<ul>\n<li>Support desecrated mods</li>\n<li>Add in-game pricer for PoE 2</li>\n<li><strong>Full Changelog</strong>: <a class="commit-link" href="https://github.com/mahhov/arevtur/compare/v8.11.0...v8.12.0"><tt>v8.11.0...v8.12.0</tt></a></li>\n</ul>'
+		},
+		{
+			'version': '8.11.0',
+			'note': '<ul>\n<li>Sort by gold fee</li>\n<li>Fix PoB weight caching when using flat-weights</li>\n<li>Fix item caching</li>\n<li>Misc improvements</li>\n<li><strong>Full Changelog</strong>: <a class="commit-link" href="https://github.com/mahhov/arevtur/compare/v8.10.0...v8.11.0"><tt>v8.10.0...v8.11.0</tt></a></li>\n</ul>'
+		}
+	]
+};
--- src/updateCheck/updateCheck.js
+++ src/updateCheck/updateCheck.js
@@ -10,9 +10,6 @@ ipcMain.handle('request-current-version', () => app.getVersion());
 ipcMain.handle('request-check-updates', () => updater.checkForUpdate());
 ipcMain.handle('request-update', () => updater.updateAndRestart());
 
-let window = new ElectronWindow(`Check for updates`, path.resolve(__dirname, 'updateCheck.html'),
-	400, 400, false);
+let window = new ElectronWindow(`Check for updates`, path.resolve(__dirname, 'updateCheck.html'), 400, 400, false);
 
 module.exports = window;
-
-// todo[blocking] updater not working
