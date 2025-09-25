70b9fc0b408fdf52486a995c9dfd6c671e816a9b
mahhov1 <mahhov1@gmail.com>
2025-09-25 09:09:30 -0700

    move keysnippets to mouse

--- src/keySnippet2/keySnippet2.js
+++ src/keySnippet2/keySnippet2.js
@@ -1,4 +1,4 @@
-const {ipcMain} = require('electron');
+const {ipcMain, screen} = require('electron');
 const path = require('path');
 const ElectronWindow = require('../services/ElectronWindow');
 
@@ -14,10 +14,19 @@ ipcMain.handle('key-snippet', async (event, arg) => {
 	let window = await windowPromise.window;
 	switch (arg) {
 		case 'show':
+			// todo[blocking] size to fit contents
+			let width = 400;
+			let height = 400;
+			let mouse = screen.getCursorScreenPoint();
+			let bounds = screen.getPrimaryDisplay().bounds;
+			let x = Math.min(mouse.x, bounds.x + bounds.width - width);
+			let y = Math.min(mouse.y, bounds.y + bounds.height - height);
+			window.setBounds({x, y, width, height});
 			window.show();
 			window.focus();
 			clearInterval(hideInterval);
-			hideInterval = setTimeout(async () => window.hide(), 3000);
+			if (!window.webContents.isDevToolsOpened())
+				hideInterval = setTimeout(async () => window.hide(), 3000);
 			break;
 		case 'close':
 			if (!window.webContents.isDevToolsOpened())
